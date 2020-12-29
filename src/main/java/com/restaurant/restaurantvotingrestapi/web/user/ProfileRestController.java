61da53277bb421ecac65c0135c02d7b469042543
space125 <kurilov@list.ru>
2020-12-29 16:53:16 +0300

    Created ProfileRestController.java

---
+++ src/main/java/com/restaurant/restaurantvotingrestapi/web/user/ProfileRestController.java
@@ -0,0 +1,92 @@
+package com.restaurant.restaurantvotingrestapi.web.user;
+
+import com.restaurant.restaurantvotingrestapi.dto.UserTo;
+import com.restaurant.restaurantvotingrestapi.model.User;
+import com.restaurant.restaurantvotingrestapi.model.Vote;
+import com.restaurant.restaurantvotingrestapi.util.UserUtil;
+import com.restaurant.restaurantvotingrestapi.util.ValidationUtil;
+import com.restaurant.restaurantvotingrestapi.web.AuthUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @author Ivan Kurilov on 29.12.2020
+ */
+
+@RestController
+@RequestMapping(value = ProfileRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+@CacheConfig(cacheNames = "users")
+public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
+
+    @GetMapping
+    public HttpEntity<User> get(@AuthenticationPrincipal AuthUser authUser) {
+        return super.get(authUser.id());
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@AuthenticationPrincipal AuthUser authUser) {
+        super.delete(authUser.id());
+    }
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    @CacheEvict(allEntries = true)
+    public ResponseEntity<User> register(@Valid @RequestBody UserTo userTo) {
+        log.info("register {}", userTo);
+        ValidationUtil.checkNew(userTo);
+        User created = prepareToSave(UserUtil.createNewFromTo(userTo));
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL).build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
+    @CacheEvict(allEntries = true)
+    public void update(@RequestBody UserTo userTo, @AuthenticationPrincipal AuthUser authUser) throws BindException {
+        validateBeforeUpdate(userTo, authUser.id());
+        User user = userRepository.getExisted(userTo.id());
+        prepareToSave(UserUtil.updateFromTo(user, userTo));
+    }
+
+    @GetMapping("/with-votes")
+    public ResponseEntity<User> getWithVotes(@AuthenticationPrincipal AuthUser authUser) {
+        return ResponseEntity.of(userRepository.getWithVotes(authUser.id()));
+    }
+
+    @GetMapping("/votes")
+    public List<Vote> getAllVotes(@AuthenticationPrincipal AuthUser authUser){
+        return super.getAllVotes(authUser.id());
+    }
+
+    @GetMapping("/votes/by")
+    public Vote getVoteByDate(@AuthenticationPrincipal AuthUser authUser,
+                              @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate date){
+        return super.getVoteByDate(authUser.id(), date);
+    }
+
+    @GetMapping("/votes/today")
+    public Vote getVoteToday(@AuthenticationPrincipal AuthUser authUser){
+        return super.getVoteToday(authUser.id());
+    }
+}
